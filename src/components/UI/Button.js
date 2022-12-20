0168933f1132963f66e78cdcb490d4d4ac4fe203
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-12-20 23:37:59 +0100

    Implement add new User function

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = () => {
   return (
-    <div className='mt-8 bg-purple-800 w-fit text-white font-bold p-2 rounded-md'>
+    <button className='mt-8 bg-purple-800 w-fit text-white font-bold p-2 rounded-md'>
       Add User
-    </div>
+    </button>
   )
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button';
 
-const AddUser = () => {
+const AddUser = (props) => {
+  const [newUser, setNewUser] = useState('');
+  const [newAge, setNewAge] = useState('');
+  const submitHandler = event => {
+    event.preventDefault();
+    props.onNewUser(newUser, newAge)
+  }
+
+  const buttonClickHandler = (e) => {
+    console.log(e);
+  }
+
+  const usernameChangeHandler = event => {
+    setNewUser(event.target.value);
+    console.log(event.target.value);
+
+  }
+
+  const ageChangeHandler = event => {
+    setNewAge(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <div className="w-2/4 bg-white rounded-md p-4 flex mx-auto mt-12">
-      <form className='flex flex-col w-full'>
+      <form className='flex flex-col w-full' onSubmit={submitHandler}>
         <label htmlFor='Username' className='font-bold'>Username</label>
-        <input type='text' title='username' placeholder='Enter Username' id='username' className='p-2 border border-solid border-slate-400' />
+        <input type='text' title='username' value={props.username} onChange={usernameChangeHandler} placeholder='Enter Username' id='username' className='p-2 border border-solid border-slate-400' />
         <label htmlFor='Age' className='mt-4 font-bold'>Age (Years)</label>
-        <input type='number' if='age' title='age' placeholder='Age in years' className='p-2 border border-solid border-slate-400' />
-        <Button type='submit'>Add User</Button>
+        <input type='number' id='age' title='age' onChange={ageChangeHandler} placeholder='Age in years' className='p-2 border border-solid border-slate-400' />
+        <Button type='submit' onClick={buttonClickHandler} />
       </form>
     </div>
   )
